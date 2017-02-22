bf455eb5fddab57e3b64bc294c8a20c62e444c67
sabrah06 <sabrah06@mail.bbk.ac.uk>
2017-02-22 21:16:34 +0000

    Initial commit

---
+++ app/src/main/java/com/Questions.java
@@ -0,0 +1,31 @@
+package com;
+
+/**
+ * Created by Sheena on 23/01/2017.
+ */
+
+public class Questions {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Questions(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
